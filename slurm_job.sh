77ff97c8b82a5bf4af1fd22ce8d470bfe53534f2
Mohamed Saad <60142059+mms100@users.noreply.github.com>
2025-06-10 12:54:45 +0200

    Update slurm_job.sh

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=nextflow_pbmc     # Job name
 #SBATCH --mail-type=END,FAIL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --output=Path/to/logs/output.%J.%x.txt
-#SBATCH --error=Path/to/logs/error.%J.%x.txt
+#SBATCH --output=Path/to/scRNA-seq_nextflow_DEG_pipeline/log/output.%J.%x.txt
+#SBATCH --error=Path/to/scRNA-seq_nextflow_DEG_pipeline/log/error.%J.%x.txt
 ### Time to execute
 #SBATCH --time=96:00:00
 
@@ -18,8 +18,8 @@ module load R/4.2.3
 
 
 # Run pipeline with batch
-//path/to/nextflow_excutor run path/to/main.nf  --object "/path/to/seurat_object.rds" --cond1 "g1" --cond2 "g2" --annotation "RNA_snn_res.0.8" --cond_colname "groups"  --batch_colname "letter.idents" --output_1 "Pbmc_batch"
+//path/to/nextflow_excutor run path/to/scRNA-seq_nextflow_DEG_pipeline/main.nf  --object "/path/to/seurat_object.rds" --cond1 "g1" --cond2 "g2" --annotation "RNA_snn_res.0.8" --cond_colname "stage"  --batch_colname "library_name" --output_1 "batch_on"
 
 
 # Run pipeline without batch
-#//path/to/nextflow_excutor run path/to/main.nf  --object "/path/to/seurat_object.rds" --cond1 "g1" --cond2 "g2" --annotation "RNA_snn_res.0.8" --cond_colname "groups"  --output_1 "Pbmc_no_batch"
+#//path/to/nextflow_excutor run path/to/scRNA-seq_nextflow_DEG_pipeline/main.nf  --object "/path/to/seurat_object.rds" --cond1 "g1" --cond2 "g2" --annotation "RNA_snn_res.0.8" --cond_colname "stage"  --output_1 "batch_off"
