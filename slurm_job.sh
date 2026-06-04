242b9463a15d7b0ad0ac04fff741499b24480ba4
Mohamed Saad <60142059+mms100@users.noreply.github.com>
2026-06-04 10:09:12 +0200

    Update slurm_job.sh

--- slurm_job.sh
+++ slurm_job.sh
@@ -18,7 +18,7 @@ module load R/4.4.1
 
 
 # Run pipeline without batch
-//scRNA-seq_nextflow_DEG_pipeline/nextflow \
+//path/to/executor/nextflow \
     run /scRNA-seq_nextflow_DEG_pipeline/main.nf \
     --results_dir "/scRNA-seq_nextflow_DEG_pipeline/output_WO_batch/"  \
     --object "/scRNA-seq_nextflow_DEG_pipeline/pbmc_object.Rds" \
